bac235a0a6173a7a7c8cfb86c606601a1edc85fb
darmen26-dec <dariamendelewska@gmail.com>
2023-12-18 16:17:24 +0100

    update changes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -64,7 +64,6 @@ const ContactForm = () => {
           type="tel"
           name="number"
           autoComplete="off"
-          pattern="(\+?\d{1,4}[-.\s]?)?(\(?\d{1,3}?\)?[-.\s]?)?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={formData.number}
